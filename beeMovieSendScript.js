a6f8c7e02f135ee2abf52e09818bbc0498bc348f
Rofen1 <rofen1@protonmail.com>
2021-10-19 12:03:38 -0300

    Correção do codigo que envia o roteiro do filme Bee Movie

--- beeMovieSendScript.js
+++ beeMovieSendScript.js
@@ -1,28 +1,29 @@
-function enviarScript(scriptText) {
-	const lines = scriptText.split('\n');
+function enviarScript(scriptText){
+    const lines = scriptText.split("\n"); 
 
-	let i = 0;
+    let i = 0;       
 
-	setInterval(() => {
-		if (i >= lines.length) return;
+    setInterval(() => { 
+        if(i >= lines.length) return
 
-		if (lines[i].trim() != '') {
-			console.log(lines[i]);
+        if(lines[i].trim() != ''){ 
+            console.log(lines[i]); 
+    
+            window.InputEvent = window.Event || window.InputEvent;
+    
+            const event = new InputEvent('input', { bubbles: true });
+    
+            const textbox = document.querySelector('#main div._13NKt');
+    
+            textbox.textContent = lines[i];
+    
+            textbox.dispatchEvent(event);
+    
+            document.querySelector("#main span[data-testid=send]").click();                   
 
-			window.InputEvent = window.Event || window.InputEvent;
-
-			const event = new InputEvent('input', { bubbles: true });
-
-			const textbox = document.querySelector('div._2_1wd[data-tab="6"]');
-
-			textbox.textContent = lines[i];
-
-			textbox.dispatchEvent(event);
-
-			document.querySelector('button._1E0Oz').click();
-		}
-		i++;
-	}, 250);
+        }
+        i++;
+    }, 250)
 }
 
 enviarScript(`
